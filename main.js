5f7ec2ac1bf1d589a5568fa1a89c9c7de0539eac
wiliam <angularsprings403@gmail.com>
2023-05-04 14:33:02 +0700

    Belajar Dasar Bahasa Pemrograman Javascript

---
+++ main.js
@@ -0,0 +1,78 @@
+// // // alert('Semangat Belajar Javascriptnya :D')
+
+// // // console.log("javascript console log")
+
+// // // prompt("Masukan Usia Anda")
+
+
+// // // var usia = 25
+// // // console.log('Usia Kamu Adalah ' + usia + ' Tahun')
+
+// // // let usia = prompt("Berapa Usia Anda Sekarang ? ")
+// // // alert("Usia Anda Sekarang Adalah " + usia)
+
+
+// // // if(asmara == null)
+// // // {
+// // //     asmara = 'Jomblo'
+// // // }else{
+// // //     asmara = 'Tidak Jomblo'
+// // // }
+
+// // let nama = 'Andrew Napitupulu'
+// // let usia = 25
+// // let tinggiBadan = 175
+// // let beratBadan
+// // let asmara = 2
+// // beratBadan = 55
+
+// // let saldoAwal = 100000
+// // let saldoAkhir = 20000
+// // const hutang = 25000
+// // const totalSaldo = saldoAwal + saldoAkhir - hutang
+
+// // switch (asmara) {
+// //     case 1:
+// //         asmara = 'Punya 1 Pacar'
+// //         break
+// //     case 2:
+// //         asmara = 'Punya Lebih Dari 1 Pacar'
+// //         break
+// //     default:
+// //         asmara = 'Belum Punya Pacar'
+// //         break
+// // }
+
+// // alert(`Nama Saya ${nama}, Usia Saya ${25} Tahun, Tinggi Badan ${tinggiBadan}cm, Berat Badan ${beratBadan}kg Dan Sekarang ${asmara}`)
+
+// // alert(`Saldo Awal Saya Adalah Rp.${saldoAwal}, Saldo Akhir Saya Rp.${saldoAkhir} dan semua total Saldo saya adalah Rp.${totalSaldo}`)
+
+// let namaGuru = ['andrew','ara', 'putri']
+
+// namaGuru.shift()
+// namaGuru.pop()
+
+// alert(namaGuru[0])
+
+
+// const namaGuru = ['Andrew','Ara', 'Wiliam', 'Putri']
+// // console.log(namaGuru.length)
+
+// for (let i = 0; i < namaGuru.length; i++) {
+//     console.log(namaGuru[i])
+    
+// }
+
+// let i = 0
+// while(i < 10)
+// {
+//     i++
+//     console.log('andrew')
+// }
+
+// let i = 0
+
+// do{
+//     i++
+//     console.log('andrew')
+// }while (i < 5) 
---
+++ tugas/tugas_pertama.js
@@ -0,0 +1,42 @@
+// TUGAS NOMOR SATU
+
+// let saldoAkhir = prompt(`Masukan Saldo Akhir Anda`)
+
+// alert(`Saldo Terakhir Anda Sebesar Rp. ${saldoAkhir}. `)
+
+
+// TUGAS NOMOR KEDUA
+
+
+// let hari = new Date().getDay()
+
+// hari = hari
+
+// switch (hari) {
+//     case 1:
+//         hari = 'Senin'
+//         break;
+//     case 2:
+//         hari = 'Selasa'
+//         break;
+//     case 3:
+//         hari = 'Rabu'
+//         break;
+//     case 4:
+//         hari = 'Kamis'
+//         break;
+//     case 5:
+//         hari = 'Jumat'
+//         break;
+//     case 6:
+//         hari = 'Sabtu'
+//         break;
+//     case 7:
+//         hari = 'Minggu'
+//         break;
+//     default:
+//         hari = 'Tidak Ada Hari'
+//         break;
+// }
+
+// alert(`Hari Di PC/Laptop Anda Sekarang Adalah Hari ${hari}.`)
